d124d05c71b734f44c22c0c16b3699f6f0a227cf
Shubham <shubhamdandekar20@gmail.com>
2020-10-09 15:51:34 +0530

    Updates

---
+++ precache-manifest.b4b99aa34e9a097dfac1242d215433fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27812bca058cdd20f923088fd19688e",
+    "url": "/monster-react-app/index.html"
+  },
+  {
+    "revision": "859bd2a7cf9381fabe22",
+    "url": "/monster-react-app/static/css/main.8ca1abcc.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-react-app/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-react-app/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "859bd2a7cf9381fabe22",
+    "url": "/monster-react-app/static/js/main.5610fe8c.chunk.js"
+  },
+  {
+    "revision": "2477951810444b63e0cb",
+    "url": "/monster-react-app/static/js/runtime-main.2010ff6a.js"
+  }
+]);
